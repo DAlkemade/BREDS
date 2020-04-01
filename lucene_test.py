9b93dd0e6ca1b3174fcd51b2afd5b37d8792c764
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-04-01 15:59:09 +0200

    Implement frequency retrieval

--- lucene_test.py
+++ lucene_test.py
@@ -3,7 +3,7 @@ import mmap
 import lucene
 from java.nio.file import Paths
 from org.apache.lucene.search import IndexSearcher
-from org.apache.lucene.index import DirectoryReader
+from org.apache.lucene.index import DirectoryReader, Term
 from org.apache.lucene.store import FSDirectory
 
 import tika.parser
@@ -13,11 +13,14 @@ if __name__ == "__main__":
      file = Paths.get("D:\GitHubD\BREDS\wiki_text_index\WIKI_TEXT")
      dir = FSDirectory.open(file)
      reader = DirectoryReader.open(dir)
+
+     term = Term("contents", "tiger")
+     print(f'Tiger frequency: {reader.totalTermFreq(term)}')
+
      for i in range(0,reader.numDocs()):
           doc = reader.document(i)
           text = doc.get("contents")
           articleID  = doc.get("articleID")
-          print(text)
           # Do your pattern matching and record patterns for document
      articleID
 
