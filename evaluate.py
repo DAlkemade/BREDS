20ed872cb3e8d735584df5762c4be30856a5f3c1
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-05-01 13:00:14 +0200

    Add evaluation

---
+++ evaluate.py
@@ -0,0 +1,72 @@
+import fileinput
+from argparse import ArgumentParser
+from typing import Dict
+import re
+import numpy as np
+from breds.config import parse_objects_from_seed
+from matplotlib import pyplot as plt
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--relationships', required=True, type=str)
+
+    seeds: set = parse_objects_from_seed('data_numeric/seeds_positive.txt').union(
+        parse_objects_from_seed(
+            'data_numeric/seeds_negative.txt'))
+
+    args = parser.parse_args()
+    non_decimal = re.compile(r'[^\d.]+')
+    tuples = set()
+    for line in fileinput.input(args.relationships, openhook=fileinput.hook_encoded("utf-8")):
+        if line.startswith("instance"):
+            entity = line.split('\t')[0][10:]
+            size = float(line.split('\t')[1])
+            confidence_str = line.split('\t')[2]
+            confidence = float(non_decimal.sub('', confidence_str))
+            tuples.add((entity, size, confidence))
+
+    # Remove objects that were in OG seed
+    tuples = [tuple for tuple in tuples if tuple[0] not in seeds]
+    tuples_high_conf = [tuple for tuple in tuples if tuple[2] > .4]
+
+    # tuples_lookup = create_lookup(tuples)
+    tuples_lookup_high_conf = create_lookup(tuples_high_conf)
+    print(tuples_lookup_high_conf)
+
+
+def create_lookup(tuples) -> dict:
+    tuples_lookup: Dict[str, set] = dict()
+    for tuple in tuples:
+        entity = tuple[0]
+        size = tuple[1]
+        confidence = tuple[2]
+        new_tuple = (size, confidence)
+        try:
+            tuples_lookup[entity].add(new_tuple)
+        except KeyError:
+            tuples_lookup[entity] = {new_tuple}
+    print(f'Number of entities with at least one size: {len(tuples_lookup.keys())}')
+
+    averages = list()
+    for e, v in tuples_lookup.items():
+        s, confs = zip(*v)
+        averages.append(np.mean(s))
+
+    print(f'biggest object: {list(tuples_lookup.items())[np.argmax(averages)]}')
+    bins = np.linspace(0,max(averages),50)
+    plt.hist(averages, bins=bins)
+    plt.xlabel('Mean object size')
+    plt.title('All objects')
+    plt.show()
+    bins = np.linspace(0, 20, 50)
+    plt.hist(averages, bins=bins)
+    plt.title('Small objects zoom')
+    plt.xlabel('Mean object size')
+    plt.show()
+
+    return tuples_lookup
+
+
+if __name__ == "__main__":
+    main()
