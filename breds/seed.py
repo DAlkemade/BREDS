85b71f7f5d4cabd9ff9114ca7b5f484ef9741d04
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-05-01 11:26:48 +0200

    Avoid duplicate sizes in seed

--- breds/seed.py
+++ breds/seed.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 
 __author__ = "David S. Batista"
 __email__ = "dsbatista@inesc-id.pt"
@@ -7,10 +7,10 @@ __email__ = "dsbatista@inesc-id.pt"
 class Seed(object):
     def __init__(self, _e1, _e2: float):
         self.e1 = _e1
-        self.sizes: List[float] = [_e2]
+        self.sizes: Set[float] = [_e2]
 
     def add_size(self, size: float):
-        self.sizes.append(size)
+        self.sizes.add(size)
 
     def __hash__(self):
         return hash(self.e1) ^ hash(self.sizes)
