9b34ffb71606b54e321908868989d8654e055748
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-05-01 11:33:17 +0200

    Make sizes a set

--- breds/seed.py
+++ breds/seed.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class Seed(object):
     def __init__(self, _e1, _e2: float):
         self.e1 = _e1
-        self.sizes: Set[float] = [_e2]
+        self.sizes: Set[float] = {_e2}
         logger.info(
             f"Created new seed {_e1} {_e2}")
 
