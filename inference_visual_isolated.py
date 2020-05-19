806d4d97219d9c29ad07926c1ce9f6fc387d34d3
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-05-19 12:09:18 +0200

    Handle config with Box and yml

--- inference_visual_isolated.py
+++ inference_visual_isolated.py
@@ -5,6 +5,8 @@ from argparse import ArgumentParser
 from datetime import datetime
 from typing import List
 
+import yaml
+from box import Box
 from logging_setup_dla.logging import set_up_root_logger
 from visual_size_comparison.config import VisualConfig
 from visual_size_comparison.propagation import build_cooccurrence_graph, Pair, VisualPropagation
@@ -14,26 +16,17 @@ from breds.config import Config
 set_up_root_logger(f'INFERENCE_VISUAL_{datetime.now().strftime("%d%m%Y%H%M%S")}', os.path.join(os.getcwd(), 'logs'))
 logger = logging.getLogger(__name__)
 
-
 def main():
-    parser = ArgumentParser()
-    parser.add_argument('--test_pairs', type=str, required=True)
-    parser.add_argument('--configuration', type=str, required=True)
-    parser.add_argument('--seeds_file', type=str, required=True)
-    parser.add_argument('--negative_seeds', type=str, required=True)
-    parser.add_argument('--similarity', type=float, required=True)
-    parser.add_argument('--confidence', type=float, required=True)
-    parser.add_argument('--objects', type=str, required=True)
-    parser.add_argument('--cache_config_fname', type=str, required=True)
-    parser.add_argument('--vg_objects', type=str, required=True)
-    parser.add_argument('--vg_objects_anchors', type=str, required=True)
-    args = parser.parse_args()
-    test_pairs_fname = args.test_pairs
+    with open("config.yml", "r") as ymlfile:
+        cfg = Box(yaml.safe_load(ymlfile))
+        # cfg = Box(yaml.safe_load(ymlfile), default_box=True, default_box_attr=None)
+    test_pairs_fname = cfg.path.test_pairs
 
     # TODO check whether the objects aren't in the bootstrapped objects
-    visual_config = VisualConfig(args.vg_objects, args.vg_objects_anchors)
-    config = Config(args.configuration, args.seeds_file, args.negative_seeds, args.similarity, args.confidence,
-                    args.objects, visual_config)
+    visual_config = VisualConfig(cfg.path.vg_objects, cfg.path.vg_objects_anchors)
+    config = Config(cfg.path.configuration, cfg.path.seeds_file, cfg.path.negative_seeds, cfg.thresholds.similarity,
+                    cfg.thresholds.confidence,
+                    cfg.path.objects, visual_config)
 
     visual_config = config.visual_config
     objects = list(visual_config.entity_to_synsets.keys())
