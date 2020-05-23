0481de572ca066df4c119b279b9670118005352f
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-05-23 11:16:47 +0200

    Change log prio

--- breds/breds_inference.py
+++ breds/breds_inference.py
@@ -79,7 +79,7 @@ def predict_sizes(all_sizes: dict, objects: list, cfg: BackoffSettings) -> Dict[
         # except ValueError:
         #     direct_highest_confidence = None
         size_direct = predict_point(True, [t.e2 for t in directs])
-        logger.info(f'Size direct: {size_direct}')
+        logger.debug(f'Size direct: {size_direct}')
 
         # TODO instead of taking means, maybe take the mean of the MAX for each hyponym, hypernym, etc
         hyponym_mean = weighted_tuple_mean(hyponyms)
