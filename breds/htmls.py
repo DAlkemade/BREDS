27bda542ea85ee82ba269af4f1fba02e37fd19af
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-06-07 19:50:40 +0200

    Use new event loop

--- breds/htmls.py
+++ breds/htmls.py
@@ -15,7 +15,7 @@ def scrape_htmls(html_fname: str, names: list):
     logger.info(f'Retrieving urls for {len(names)} objects')
     urls = create_or_update_results('urls.pkl', queries, names)
 
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     htmls_lookup = html_scraper.create_or_update_urls_html(html_fname, names, urls, loop)
-    loop.close()
     return htmls_lookup
